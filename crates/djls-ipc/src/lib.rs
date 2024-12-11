74f4d7203abc44d105750f77e931f2fa494712ff
Josh <josh@joshthomas.dev>
2024-12-11 16:18:53 -0600

    remove unneeded client and server code

--- crates/djls-ipc/src/lib.rs
+++ crates/djls-ipc/src/lib.rs
@@ -1,11 +1,7 @@
-mod client;
 mod process;
-mod server;
 mod transport;
 
-pub use client::Client;
 pub use process::PythonProcess;
-pub use server::Server;
 pub use transport::parse_json_response;
 pub use transport::parse_raw_response;
 pub use transport::JsonResponse;
