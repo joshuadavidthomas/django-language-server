4c10afb602150882375f40fc3750028c52065398
Josh Thomas <josh@joshthomas.dev>
2024-12-11 10:23:35 -0600

    create djls-cli and migrate serving LSP to it (#20)

---
+++ crates/djlc-cli/src/main.rs
@@ -0,0 +1,24 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Start the LSP server
+    Serve,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Serve => djls_server::serve().await?,
+    }
+
+    Ok(())
+}
