77d4b4fb8e29ff4035aa0c2d850232210f0b82d1
Josh <josh@joshthomas.dev>
2024-12-18 18:52:12 -0600

    fix macros

--- crates/djls-ipc/src/messages/macros.rs
+++ crates/djls-ipc/src/messages/macros.rs
@@ -34,66 +34,64 @@ macro_rules! define_messages {
             }
         )+
 
-pub fn all_message_schemas() -> Result<(String, Vec<(String, schemars::schema::RootSchema)>), SchemaError> {
-    let mut combined_schema = schemars::schema::RootSchema {
-        meta_schema: Some("http://json-schema.org/draft-07/schema#".to_string()),
-        schema: schemars::schema::Schema::Object(schemars::schema::SchemaObject {
-            reference: Some("#/definitions/Messages".to_string()),  // Or another type
-            ..Default::default()
-        }).into(),
-        definitions: std::collections::BTreeMap::new(),
-    };
+        pub fn all_message_schemas() -> Result<(String, Vec<(String, schemars::schema::RootSchema)>), SchemaError> {
+            let mut combined_schema = schemars::schema::RootSchema {
+                meta_schema: Some("http://json-schema.org/draft-07/schema#".to_string()),
+                schema: schemars::schema::Schema::Object(schemars::schema::SchemaObject {
+                    reference: Some("#/definitions/Messages".to_string()),
+                    ..Default::default()
+                }).into(),
+                definitions: std::collections::BTreeMap::new(),
+            };
 
-    // Add our core types with proper schema definitions
-    combined_schema.definitions.insert(
-        "ErrorResponse".to_string(),
-        schemars::schema::Schema::Object(schemars::schema_for!(ErrorResponse).schema)
-    );
-    combined_schema.definitions.insert(
-        "Messages".to_string(),
-        schemars::schema::Schema::Object(schemars::schema_for!(Messages).schema)
-    );
-    combined_schema.definitions.insert(
-        "Request".to_string(),
-        schemars::schema::Schema::Object(schemars::schema_for!(GenericRequest).schema)
-    );
-    combined_schema.definitions.insert(
-        "Response".to_string(),
-        schemars::schema::Schema::Object(schemars::schema_for!(GenericResponse).schema)
-    );
-
-    // Add message-specific schemas
-    $(
-        paste::paste! {
-            let name = stringify!($variant)
-                .chars()
-                .enumerate()
-                .fold(String::new(), |mut acc, (i, c)| {
-                    if i > 0 && c.is_uppercase() {
-                        acc.push('_');
-                    }
-                    acc.extend(c.to_lowercase());
-                    acc
-                });
-
-            let message_schema = schemars::schema_for!([<$variant Message>]);
-            combined_schema.definitions.extend(message_schema.definitions);  // Include nested definitions
             combined_schema.definitions.insert(
-                format!("{}_message", name),
-                schemars::schema::Schema::Object(message_schema.schema)
+                "ErrorResponse".to_string(),
+                schemars::schema::Schema::Object(schemars::schema_for!(ErrorResponse).schema)
             );
+            combined_schema.definitions.insert(
+                "Messages".to_string(),
+                schemars::schema::Schema::Object(schemars::schema_for!(Messages).schema)
+            );
+            combined_schema.definitions.insert(
+                "Request".to_string(),
+                schemars::schema::Schema::Object(schemars::schema_for!(GenericRequest).schema)
+            );
+            combined_schema.definitions.insert(
+                "Response".to_string(),
+                schemars::schema::Schema::Object(schemars::schema_for!(GenericResponse).schema)
+            );
+
+            $(
+                paste::paste! {
+                    let name = stringify!($variant)
+                        .chars()
+                        .enumerate()
+                        .fold(String::new(), |mut acc, (i, c)| {
+                            if i > 0 && c.is_uppercase() {
+                                acc.push('_');
+                            }
+                            acc.extend(c.to_lowercase());
+                            acc
+                        });
+
+                    let message_schema = schemars::schema_for!([<$variant Message>]);
+                    combined_schema.definitions.extend(message_schema.definitions);
+                    combined_schema.definitions.insert(
+                        format!("{}_message", name),
+                        schemars::schema::Schema::Object(message_schema.schema)
+                    );
+                }
+            )+
+
+            // Set the root schema to be an object with all our definitions
+            // This gets rid of the random Model/RootModel from appearing
+            combined_schema.schema = schemars::schema::Schema::Object(schemars::schema::SchemaObject {
+                ..Default::default()
+            }).into();
+
+            Ok((file!().to_string(), vec![
+                ("schema".to_string(), combined_schema)
+            ]))
         }
-    )+
-
-    // Set the root schema to be an object with all our definitions
-    combined_schema.schema = schemars::schema::Schema::Object(schemars::schema::SchemaObject {
-        // Maybe specify the root object structure here
-        ..Default::default()
-    }).into();
-
-    Ok((file!().to_string(), vec![
-        ("schema".to_string(), combined_schema)
-    ]))
-}
     };
 }
