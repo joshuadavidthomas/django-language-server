c5f84849933b171c0631e5cbb5d3ca594a7bea34
Josh <josh@joshthomas.dev>
2024-12-24 13:27:14 -0600

    remove notifier from debug task

--- crates/djls-server/src/tasks.rs
+++ crates/djls-server/src/tasks.rs
@@ -1,23 +1,15 @@
-use crate::notifier::Notifier;
 use anyhow::Result;
 use djls_worker::Task;
-use std::sync::Arc;
 use std::time::Duration;
-use tower_lsp::lsp_types::MessageType;
 
 pub struct DebugTask {
     pub message: String,
     pub delay: Duration,
-    pub notifier: Arc<Box<dyn Notifier>>,
 }
 
 impl DebugTask {
-    pub fn new(message: String, delay: Duration, notifier: Arc<Box<dyn Notifier>>) -> Self {
-        Self {
-            message,
-            delay,
-            notifier,
-        }
+    pub fn new(message: String, delay: Duration) -> Self {
+        Self { message, delay }
     }
 }
 
@@ -27,11 +19,6 @@ impl Task for DebugTask {
     fn run(&self) -> Result<Self::Output> {
         std::thread::sleep(self.delay);
         let result = format!("Debug task completed: {}", self.message);
-
-        // Log the result
-        self.notifier
-            .log_message(MessageType::INFO, &result)
-            .unwrap_or_default();
 
         Ok(result)
     }
