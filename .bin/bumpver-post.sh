a91e89ff677a2e745269a458ee5407d8d902c863
Josh <josh@joshthomas.dev>
2025-01-03 12:06:28 -0600

    use pep440

--- .bin/bumpver-post.sh
+++ .bin/bumpver-post.sh
@@ -2,20 +2,30 @@
 
 set -euo pipefail
 
+get_version() {
+        uv run --with bumpver bumpver show --no-fetch | grep -Po '^PEP440         : \K.*'
+}
+
 update_changelog() {
         local repo_url
         repo_url=$(git remote get-url origin | tr -d '\n' | sed 's/\.git$//')
 
-        sed -i "0,/## \[Unreleased\]/s/## \[Unreleased\]/## [$BUMPVER_NEW_VERSION]/" CHANGELOG.md
-        sed -i "/## \[$BUMPVER_NEW_VERSION\]/i ## [Unreleased]\n" CHANGELOG.md
-        echo "[$BUMPVER_NEW_VERSION]: $repo_url/releases/tag/v$BUMPVER_NEW_VERSION" >>CHANGELOG.md
-        sed -i "s|\[unreleased\]: .*|[unreleased]: $repo_url/compare/v$BUMPVER_NEW_VERSION...HEAD|" CHANGELOG.md
+        local version
+        version=$(get_version)
+
+        sed -i "0,/## \[Unreleased\]/s/## \[Unreleased\]/## [$version]/" CHANGELOG.md
+        sed -i "/## \[$version\]/i ## [Unreleased]\n" CHANGELOG.md
+        echo "[$version]: $repo_url/releases/tag/v$version" >>CHANGELOG.md
+        sed -i "s|\[unreleased\]: .*|[unreleased]: $repo_url/compare/v$version...HEAD|" CHANGELOG.md
 
         git add CHANGELOG.md
-        git commit -m "update CHANGELOG for version $BUMPVER_NEW_VERSION"
+        git commit -m "update CHANGELOG for version $version"
 }
 
 update_uvlock() {
+        local version
+        version=$(get_version)
+
         uv lock
 
         if ! git status --porcelain | grep -q "uv.lock"; then
@@ -24,7 +34,7 @@ update_uvlock() {
         fi
 
         git add uv.lock
-        git commit -m "update uv.lock for version $BUMPVER_NEW_VERSION"
+        git commit -m "update uv.lock for version $version"
 }
 
 main() {
