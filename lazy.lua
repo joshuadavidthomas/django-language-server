162c43e1882c7e98942333a066b6f2affd03be95
Josh <josh@joshthomas.dev>
2025-01-03 07:04:17 -0600

    adjust lazy

--- lazy.lua
+++ lazy.lua
@@ -1,5 +1,11 @@
 return {
-  name = "djls.nvim",
-  dir = "editors/nvim",
-  opts = {},
+  "joshuadavidthomas/django-language-server",
+  config = function(plugin)
+    vim.notify(plugin.dir)
+    vim.opt.rtp:append(plugin.dir .. "/editors/nvim")
+    require("djls").setup()
+  end,
+  dependencies = {
+    "neovim/nvim-lspconfig",
+  },
 }
