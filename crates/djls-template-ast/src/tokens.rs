dcd0cabd6acb9a0a4cc79fdc30a17472f5d0270d
Josh <josh@joshthomas.dev>
2025-01-06 13:51:34 -0600

    check

--- crates/djls-template-ast/src/tokens.rs
+++ crates/djls-template-ast/src/tokens.rs
@@ -1,5 +1,4 @@
 use serde::Serialize;
-use std::fmt;
 use std::ops::{Deref, DerefMut};
 
 #[derive(Clone, Debug, Serialize, PartialEq)]
@@ -33,56 +32,10 @@ impl TokenType {
             | TokenType::StyleTagOpen(s)
             | TokenType::StyleTagClose(s)
             | TokenType::Text(s) => Some(s.len()),
-            TokenType::Comment(content, start, end) => match end {
-                Some(end) => Some(start.len() + 1 + content.len() + 1 + end.len()),
-                None => Some(start.len() + 1 + content.len()),
-            },
-            TokenType::Whitespace(len) => Some(*len),
+            TokenType::Comment(content, _, _) => Some(content.len()),
+            TokenType::Whitespace(n) => Some(*n),
             TokenType::Newline => Some(1),
-            TokenType::Eof => None,
-        }
-    }
-
-    pub fn lexeme(&self) -> &str {
-        match self {
-            TokenType::DjangoBlock(s)
-            | TokenType::DjangoVariable(s)
-            | TokenType::HtmlTagOpen(s)
-            | TokenType::HtmlTagClose(s)
-            | TokenType::HtmlTagVoid(s)
-            | TokenType::ScriptTagOpen(s)
-            | TokenType::ScriptTagClose(s)
-            | TokenType::StyleTagOpen(s)
-            | TokenType::StyleTagClose(s)
-            | TokenType::Text(s) => s,
-            TokenType::Comment(content, _, _) => content,
-            TokenType::Whitespace(_) => " ",
-            TokenType::Newline => "\n",
-            TokenType::Eof => "",
-        }
-    }
-}
-
-impl fmt::Display for TokenType {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        use TokenType::*;
-
-        match self {
-            Comment(content, start, end) => match end {
-                Some(end) => write!(f, "{} {} {}", start, content, end),
-                None => write!(f, "{} {}", start, content),
-            },
-            DjangoBlock(s) => write!(f, "{{% {} %}}", s),
-            DjangoVariable(s) => write!(f, "{{{{ {} }}}}", s),
-            Eof => Ok(()),
-            HtmlTagOpen(s) | ScriptTagOpen(s) | StyleTagOpen(s) => write!(f, "<{}>", s),
-            HtmlTagClose(s) => write!(f, "</{}>", s),
-            HtmlTagVoid(s) => write!(f, "<{}/>", s),
-            Newline => f.write_str("\n"),
-            ScriptTagClose(_) => f.write_str("</script>"),
-            StyleTagClose(_) => f.write_str("</style>"),
-            Text(s) => f.write_str(s),
-            Whitespace(len) => f.write_str(&" ".repeat(*len)),
+            TokenType::Eof => Some(0),
         }
     }
 }
@@ -103,15 +56,43 @@ impl Token {
         }
     }
 
-    pub fn lexeme_from_source<'a>(&self, source: &'a str) -> Option<&'a str> {
-        match (self.start, self.token_type.len()) {
-            (Some(start), Some(len)) => Some(&source[start..start + len]),
-            _ => None,
+    pub fn lexeme(&self) -> String {
+        match &self.token_type {
+            TokenType::Comment(_, start, end) => match end {
+                Some(end) => format!("{} {} {}", start, self.content(), end),
+                None => format!("{} {}", start, self.content()),
+            },
+            TokenType::DjangoBlock(_) => format!("{{% {} %}}", self.content()),
+            TokenType::DjangoVariable(_) => format!("{{{{ {} }}}}", self.content()),
+            TokenType::Eof => String::new(),
+            TokenType::HtmlTagOpen(_)
+            | TokenType::ScriptTagOpen(_)
+            | TokenType::StyleTagOpen(_) => format!("<{}>", self.content()),
+            TokenType::HtmlTagClose(_)
+            | TokenType::StyleTagClose(_)
+            | TokenType::ScriptTagClose(_) => format!("</{}>", self.content()),
+            TokenType::HtmlTagVoid(_) => format!("<{}/>", self.content()),
+            TokenType::Newline | TokenType::Text(_) | TokenType::Whitespace(_) => self.content(),
         }
     }
 
-    pub fn lexeme(&self) -> &str {
-        self.token_type.lexeme()
+    pub fn content(&self) -> String {
+        match &self.token_type {
+            TokenType::Comment(s, _, _)
+            | TokenType::DjangoBlock(s)
+            | TokenType::DjangoVariable(s)
+            | TokenType::Text(s)
+            | TokenType::HtmlTagOpen(s)
+            | TokenType::HtmlTagClose(s)
+            | TokenType::HtmlTagVoid(s)
+            | TokenType::ScriptTagOpen(s)
+            | TokenType::ScriptTagClose(s)
+            | TokenType::StyleTagOpen(s)
+            | TokenType::StyleTagClose(s) => s.to_string(),
+            TokenType::Whitespace(len) => " ".repeat(*len),
+            TokenType::Newline => "\n".to_string(),
+            TokenType::Eof => "".to_string(),
+        }
     }
 
     pub fn token_type(&self) -> &TokenType {
