d2f373de3ac6c30b992d696cca04108a1b0f33fe
Josh <josh@joshthomas.dev>
2025-05-02 22:33:52 -0500

    add autocmd for setting htmldjango filetype in tests directory

--- .lazy.lua
+++ .lazy.lua
@@ -4,4 +4,12 @@ vim.lsp.config["djls"] = {
   root_markers = { "manage.py", "pyproject.toml" },
 }
 vim.lsp.enable("djls")
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*/tests/**/*.html",
+  callback = function()
+    vim.bo.filetype = "htmldjango"
+  end,
+})
+
 return {}
