e9e05a871412116de7bd58d4c9c6745be5ccc288
Josh <josh@joshthomas.dev>
2025-09-23 21:17:51 -0500

    adjust nox commands to avoid building project

--- noxfile.py
+++ noxfile.py
@@ -2,12 +2,10 @@ from __future__ import annotations
 
 import json
 import os
-import platform
 import re
 from pathlib import Path
 
 import nox
-from bumpver.version import to_pep440
 
 nox.options.default_venv_backend = "uv|virtualenv"
 nox.options.reuse_existing_virtualenvs = True
@@ -276,6 +274,8 @@ def release(session):
 
 
 def get_version(session):
+    from bumpver.version import to_pep440
+
     command = ["uv", "run", "bumpver", "update", "--dry", "--no-fetch"]
     if session.posargs:
         args = []
