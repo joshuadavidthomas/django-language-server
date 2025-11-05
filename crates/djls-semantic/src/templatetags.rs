0b4a0db9bca067cb1095d552d8f59afd32d60f8b
Josh Thomas <josh@joshthomas.dev>
2025-11-05 00:19:11 -0600

    finish migrating to tagspec v0.6.0

--- crates/djls-semantic/src/templatetags.rs
+++ crates/djls-semantic/src/templatetags.rs
@@ -3,6 +3,7 @@ mod specs;
 
 pub use builtins::django_builtin_specs;
 pub use specs::EndTag;
+pub use specs::LiteralKind;
 pub use specs::TagArg;
 pub(crate) use specs::TagArgSliceExt;
 pub use specs::TagSpec;
